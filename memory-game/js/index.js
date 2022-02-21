7c343d5018278fe2f2fa62948d3ffb24354f468c
Денис Чухліб <dchuhleb@icloud.com>
2022-02-21 02:44:14 +0200

    feat:add match functions

--- memory-game/js/index.js
+++ memory-game/js/index.js
@@ -19,30 +19,71 @@ const randomEighteen = () => {
 	  }
 	 }
  
-	res = arr1.concat(arr2)
-	return res
+	return res = arr1.concat(arr2)
  }
 
 const createCards = (el) =>{
 for(let i = 0; i < el.length; i++){
 	const card = document.createElement('img');
-	card.classList.add('back-face');
+	card.classList.add('front-face');
 	card.src = `source/img/cards/${el[i]}.jpg`;
 	card.alt = `${el[i]} card`;
-	cards[i].append(card)
+	cards[i].append(card);
+	cards[i].setAttribute('data-image', `${el[i]}`);
 	}
 }
 
 window.addEventListener('load', createCards(randomEighteen()))
 /*------------------------------flip-card------------------------------------------*/
+let hasFlippedCard = false;
+let firstCard,
+	 secondCard;
 
+const flipCard = (event) =>{
+	event.currentTarget.classList.add('flip')
 
-const flipCard = () =>{
-	console.log('click')
+	if(!hasFlippedCard){
+		hasFlippedCard = true;
+		firstCard = event.currentTarget;
+		return
+	}
+	if (event.currentTarget != firstCard){
+		hasFlippedCard = false;
+		secondCard = event.currentTarget;
+
+		checkForMatch()
+	}
+}
+/*-------------------------------match cards-----------------------------------------*/
+const checkForMatch = () =>{
+	let isMatch = firstCard.dataset.image === secondCard.dataset.image
+	console.log(hasFlippedCard)
+	isMatch ? disableCards() : unFlipCards()
+}
+
+const disableCards = () =>{
+	firstCard.removeEventListener('click', flipCard)
+	secondCard.removeEventListener('click', flipCard)
+	resetBoard()
+}
+
+const unFlipCards = () =>{
+	cards.forEach( card => card.removeEventListener('click', flipCard))
+	setTimeout(() => {
+		cards.forEach( card => card.addEventListener('click', flipCard))
+	}, 1000)
+	setTimeout(() => {
+		firstCard.classList.remove('flip')
+		secondCard.classList.remove('flip')
+	}, 1000)
+}
+
+const resetBoard = () =>{
+	[firstCard, secondCard] = [null, null]
 }
 
 cards.forEach( card => card.addEventListener('click', flipCard))
-
+/*------------------------------------------------------------------------*/
 
 
 
